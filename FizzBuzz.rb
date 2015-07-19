ce0d89335f5ba84b78d04dee94dff738e2db06e3
Patrick Morris <prmwrites@gmail.com>
2015-07-19 15:32:27 -0400

    Solved fizzbuzz

---
+++ FizzBuzz.rb
@@ -0,0 +1,13 @@
+
+1.upto(100).each do |n|
+  if (n % 3 ==0 && n % 5 == 0)
+    puts "fizzbuzz"
+  elsif  (n % 3 == 0)
+    puts "fizz"
+ elsif (n % 5 ==0)
+    puts "buzz"
+  else
+    puts n
+  end
+  end
+
